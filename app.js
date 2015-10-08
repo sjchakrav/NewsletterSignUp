81d1cdfc9f496f28796f0ef00976e319d8219d04
Sanjay Chakravorty <Sjchakrav@ufl.edu>
2015-10-08 19:24:01 -0400

    update js

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
+var formSubmitted = false,
+valid = false;
+
+var email;
+
+
+$('.msg').hide();
+
 $(document).ready(function() {
   // Regex for Validation
-  $('form input[name="email"]').blur(function () {
-      var email = $(this).val();
-      var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
-      if (re.test(email)) {
-          $('.msg').hide();
-          $('.success').show();
-          $('.btn btn-primary').addClass("selected");
-      } else {
-      $('.msg').hide();
-      $('.error').show();
-      }
+  $('form input[name="email"]').on('keyup keypress blur change', function () {
+    email = $(this).val();
+    var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
+    if (re.test(email)) {
+      $('#msg').hide();
+      valid = true;
+      $('#form button').prop('disabled', false);
+    } else {
+      $('#msg').text('Invalid email address');
+      $('#msg').show();
+      $('#form button').prop('disabled', true);
+      valid = false;
+    }
   });
 
-    $('form').on('submit', function(e) {
-      e.preventDefault();
+  $('form').on('submit', function(e) {
+    e.preventDefault();
       //add loading GIF here
-      $.post('https://protected-lake-8698.herokuapp.com/api/subscribe', email).then(function(data) {
-        console.log(data);
-        $('#div1 h2').text(data.title)
-      }).fail(function() {
-        console.log('error!');
-      });
+
+      if(valid && !formSubmitted) {
+          console.log(email);
+        formSubmitted = true;
+        $.post('https://protected-lake-8698.herokuapp.com/api/subscribe', email).then(function(data) {
+          formSubmitted = false;
+          console.log(data);
+          $('#div1 h2').text(data.title)
+        }).fail(function() {
+          console.log('error!');
+        });
+      }
     });
 });
 
